993105a0efdc3579a70295aa0c41e545da677fc1
NavinGaddam <navingaddam2@gmail.com>
2026-09-07 18:34:40 +0530

    Load Coworx branding styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,4 +4,5 @@ import App from "./App";
 import "./styles.css";
 import "./repair.css";
 import "./pano.css";
+import "./branding.css";
 createRoot(document.getElementById("root")!).render(<React.StrictMode><App/></React.StrictMode>);
