01378dc1982290227dd650e32011ffa447ed7dbc
NavinGaddam <navingaddam2@gmail.com>
2026-09-09 11:36:29 +0530

    clear

--- public/sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE="coworx-central-v5";
-self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE))});
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{if(r.ok)caches.open(CACHE).then(c=>c.put(e.request,r.clone()));return r}).catch(()=>caches.match(e.request)))});
--- src/firebase.ts
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { firebaseConfig } from "./firebaseConfig";
-
-export const isFirebaseConfigured = !Object.values(firebaseConfig).some((v) => v.includes("PASTE_"));
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
